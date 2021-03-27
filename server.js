fa9a568da1e7bb3a8e572ed00fbaa5ba45870718
Abdul Basit <chabdulbasit1122@gmail.com>
2021-03-27 18:10:42 +0500

    Cookie issue

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const express = require("express");
 const routes = require("./src/routes");
 const CONFIG = require("./config/config");
 const bodyParser = require("body-parser");
-const sgMail = require('@sendgrid/mail')
-const cookieParser = require('cookie-parser')
+const sgMail = require("@sendgrid/mail");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // Connect to Database
-require("./database")
+require("./database");
 
 // Set Sengrid API Key to send emails
 sgMail.setApiKey(CONFIG.SENGRID_API_KEY);
 
-
 const app = express();
 
-
 /**
- * CORS 
+ * CORS
  */
 const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -31,39 +30,37 @@ const allowCrossDomain = function (req, res, next) {
     next();
   }
 };
-
-app.use(allowCrossDomain);
+app.use(cors({ origin: true, credentials: true }));
+//app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/test", (req,res)=>{
-
+app.get("/test", (req, res) => {
   const msg = {
-    to: 'bcsf17m547@pucit.edu.pk', // Change to your recipient
-    from: 'servicehubinc@gmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
-  sgMail.send(msg).then(()=>{
-    console.log("sent");
-  }).catch((err)=>{
-    res.send(err)
-  })
-
-})
+    to: "bcsf17m547@pucit.edu.pk", // Change to your recipient
+    from: "servicehubinc@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("sent");
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 app.use("/api/v1/", routes);
 
-
-
 app.listen(CONFIG.PORT, (err) => {
-    if (err) {
-      console.error(err); // eslint-disable-line no-console
-      return;
-    }
-    console.log(`App is running on port ${CONFIG.PORT}`); // eslint-disable-line no-console
+  if (err) {
+    console.error(err); // eslint-disable-line no-console
+    return;
+  }
+  console.log(`App is running on port ${CONFIG.PORT}`); // eslint-disable-line no-console
 });
-  
+
 module.exports = app;
-  
